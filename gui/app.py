11f5e6d01954f903010ecb47606da2b9f7edd2b5
Hanna Smach <01140716@pw.edu.pl>
2023-10-25 14:01:39 +0200

    checking password function

--- gui/app.py
+++ gui/app.py
@@ -1,16 +1,17 @@
 import sys
 sys.path.append('../')
-import pandas as pd
-from datetime import datetime
 import pickle
+from datetime import datetime
+import pandas as pd
+from argon2 import PasswordHasher
+from argon2.exceptions import VerifyMismatchError
+from PyQt5.QtSql import QSqlDatabase, QSqlQuery
 from PyQt5.QtWidgets import (
     QApplication, QWidget, QDialog, QMainWindow, QMessageBox
 )
-
-from kmodule.keystroke_module import feature_extract_method_1
+from loginForm_ui import Ui_monKEY
 from main_window_ui import Ui_Dialog
-from loginForm_ui import Ui_MainWindow
-from PyQt5.uic import loadUi
+from kmodule.keystroke_module import feature_extract_method_1
 
 
 def count_time_from_0(df):
@@ -53,16 +54,15 @@ class Window(QWidget, Ui_Dialog):
 
         print(df.head())
         va_HT = feature_extract_method_1(
-                    df, dynamic_feature='holdTime', time_feature='timeLapse', assumed_length=180, window_time=90)
+            df, dynamic_feature='holdTime', time_feature='timeLapse', assumed_length=180, window_time=90)
 
         # load model from pickle file
-        with open('model_2.pkl', 'rb') as file:  
+        with open('model_2.pkl', 'rb') as file:
             model = pickle.load(file)
 
-        # evaluate model 
+        # evaluate model
         Y = model.predict(va_HT.reshape((1, -1)))
         print("Result: ", Y)
-
 
     def connectSignalsSlots(self):
         self.startButton.clicked.connect(self.startRecording)
@@ -87,32 +87,120 @@ class Window(QWidget, Ui_Dialog):
 #         super().__init__(parent)
 #         loadUi("ui/find_replace.ui", self)
 
-class initWindow(QMainWindow, Ui_MainWindow):
+class initWindow(QMainWindow, Ui_monKEY):
 
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
+        self.rep_passwordEdit.hide()
+        self.rep_passwordLabel.hide()
         self.win = None
+        self.con = QSqlDatabase.addDatabase('QSQLITE')
+        self.con.setDatabaseName("C:\sqlite\DatabaseMonKEY.db")
         self.connectSignalsSlots()
 
+    # def resizeEvent(self, event):
+    #     print("resizing is not able now")
+    #     # QMainWindow.resizeEvent(self, event)
+
+    @staticmethod
+    def logging_failure():
+        msg = QMessageBox()
+        msg.setWindowTitle('Message')
+        msg.setStyleSheet(
+            "QLabel{min-width: 400px; min-height: 100px; font-size: 15px;}")
+        msg.setText("<p align='center'>Incorrect Username or Password.")
+        msg.exec_()
+
     def switchWindows(self):
-        print('The LOG IN button has been clicked')
-        # first check password/user
         self.hide()
         self.win = Window()
         self.win.show()
 
+    def first_check_password(self):
+        out_username = self.usernameEdit.text()
+        out_password = self.passwordEdit.text()
+        ph = PasswordHasher()
+
+        opened = self.con.open()
+        if not opened:
+            print("database not found!")
+        else:
+            query = QSqlQuery()
+            query.exec(f"""SELECT password FROM users WHERE username=('{out_username}')""")
+            query.first()
+            if query.isNull(0):
+                print('User does not exist.')
+                self.logging_failure()
+            else:
+                hashed_pass = query.value(0)
+                try:
+                    isValid = ph.verify(hashed_pass, str(out_password))
+                    self.switchWindows()
+                except VerifyMismatchError:
+                    print('Password is incorrect.')
+                    self.logging_failure()
+            self.con.close()      
+
+    def user_register(self):
+
+        out_username = self.usernameEdit.text()
+        out_password = self.passwordEdit.text()
+        rep_password = self.rep_passwordEdit.text()
+
+        opened = self.con.open()
+        if not opened:
+            print("database not found!")
+        else:
+            query_existing = QSqlQuery()
+            query_str = f"""SELECT EXISTS(SELECT 1 FROM users WHERE username=('{out_username}'))"""
+            query_existing.exec(f"""SELECT EXISTS(SELECT 1 FROM users WHERE username=('{out_username}'))""")
+            query_existing.first()
+            if query_existing.value(0):
+                print('This username exists in database.\nPlease select different name.')
+                self.logging_failure()  
+            else:
+                if out_password == rep_password:
+
+                    ph = PasswordHasher()
+                    out_hashed = ph.hash(str(out_password))
+                    query = QSqlQuery()
+                    query.exec(f"""INSERT INTO users (username, password) VALUES ('{out_username}','{out_hashed}')""")
+                    self.con.close()
+                    print('Success.')
+                    self.usernameEdit.clear()
+                    self.passwordEdit.clear()
+                    self.rep_passwordEdit.clear()
+                    self.rep_passwordEdit.hide()
+                    self.rep_passwordLabel.hide()
+                    self.loginButton.show()
+                        
+                else:
+                    print('Repeated password does not match.')
+                    self.logging_failure()
+
+    def user_preregister(self):
+        if self.loginButton.isVisible():
+
+            self.loginButton.hide()
+            self.rep_passwordEdit.show()
+            self.rep_passwordLabel.show()
+        else:
+            self.user_register()
+
     def connectSignalsSlots(self):
-        self.loginButton.clicked.connect(self.switchWindows)
-        # self.action_Exit.triggered.connect(self.close)
-        # self.action_Find_Replace.triggered.connect(self.findAndReplace)
-        # self.action_About.triggered.connect(self.about)
+        self.loginButton.clicked.connect(self.first_check_password)
+        self.registerButton.clicked.connect(self.user_preregister)
 
 
 if __name__ == "__main__":
 
     app = QApplication(sys.argv)
     init_win = initWindow()
-    # win = Window()
     init_win.show()
     sys.exit(app.exec())
+
+
+# TO DO: register FORM: change layouts, 
+# TO DO: add limits for user chars number and password limits 
+# TO DO: main WINDOW GUI: time 
